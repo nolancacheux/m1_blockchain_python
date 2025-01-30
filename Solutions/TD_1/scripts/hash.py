6bdd1e2073888ec61cb66b3d6ee3aa829aaa7c51
CACHEUX <nolan.cacheux@student.junia.com>
2025-01-30 12:23:01 +0100

    beginning of assignments 3

--- Solutions/TD_1/scripts/hash.py
+++
@@ -1,9 +0,0 @@
-# Fast hash using XOR operator
-
-def simple_hash(inputString):
-    output = 0                            # Output is initialised to 0
-    for character in inputString:         # For each character in input string...
-        output += ord(character)          # Add the character's ASCII code
-        output ^= (output * 31)           # Apply XOR with output and 31 times the output
-        output %= 2 ** 32                 # Clamp between 0 and 4294967296
-    return output
